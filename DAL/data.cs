d240cf3755d3fabb33e37cc157d61fb2bcc3b8ef
Ramya <adabalaramyajyothi@gmail.com>
2023-04-20 14:27:08 +0530

    initial commit

--- DAL/data.cs
+++ DAL/data.cs
@@ -2,7 +2,15 @@ using System;
 using ShoppingCartList;
 namespace cosmosdata
 {
-    public class data{
+   public interface mydb
+   {
+   Task<dynamic> getallitems();
+   Task<dynamic> gettitembyid(string id,string category);
+   Task<dynamic> Createitem(string id,string category);
+   Task<dynamic>updateitem(string id,string category);
+   Task<dynamic>Deleteitem(string id,string category);
+   }    
+   public class data:mydb{
         private const string DatabaseName = "ShoppingCartItems";
         private const string CollectionName = "Items";
         private readonly CosmosClient _cosmosClient;
@@ -20,23 +28,23 @@ namespace cosmosdata
            gmydata.Data=shp;
            return gmydata;
          }
-         public static async Task<dynamic> gettitembyid()
+         public static async Task<dynamic> gettitembyid(string id,string category)
          {
          
                 var item =await documentContainer.ReadItemAsync<ShoppingCartItem>(id, new Microsoft.Azure.Cosmos.PartitionKey(category));
                 return item;
          }
-         public static async Task<dynamic> Createitem()
+         public static async Task<dynamic> Createitem(string id,string category)
          {
 
             return await documentContainer.CreateItemAsync(item, new Microsoft.Azure.Cosmos.PartitionKey(item.Category));           
          }
-         public static async Task<dynamic>updateitem()
+         public static async Task<dynamic>updateitem(string id,string category)
          {
             string requestData =await new StreamReader(req.Body).ReadToEndAsync();
             return requestedData;
          }
-         public static async Task<dynamic>Deleteitem(IDocumentClient client, string id,string category)
+         public static async Task<dynamic>Deleteitem(string id,string category)
          {
             return await documentContainer.DeleteItemAsync<ShoppingCartItem>(id, new Microsoft.Azure.Cosmos.PartitionKey(category));
             
