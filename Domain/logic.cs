354e39ca60d55e45948f10054cfaadbd356861f7
Ramya <adabalaramyajyothi@gmail.com>
2023-04-20 14:38:35 +0530

    first commit

--- Domain/logic.cs
+++ Domain/logic.cs
@@ -1,34 +1,128 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-
-namespace Company.Function
+using ShoppingCartList.Models;
+using System.Dynamic;
+using System.IO;
+using System.Threading.Tasks;
+using cosmosdata;
+namespace ShoppingCartList
 {
-    public static class HttpTrigger1
+    public class ShoppingCartApi
     {
-        [FunctionName("HttpTrigger1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public ShoppingCartApi(mydb db)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+          _db=db;
+        }
+    [FunctionName("Getallshoppingcartitems")]
+    public static async IActionResult Getallemps(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getallshoppingcartitems")] HttpRequest req,
+        [CosmosDB(
+            DatabaseName,
+                CollectionName,
+                Connection ="CosmosDBConnectionString",
+                SqlQuery = "SELECT * FROM c")]
+               System.Collections.Generic.IEnumerable<ShoppingCartItem> shp,
+        ILogger log)
+    {
+        
+        log.LogInformation("Getting list of all employees ");
+        await _db.getallitems();
+        string gmessage="Retrieved all items successfully";
+        string json = Newtonsoft.Json.JsonConvert.SerializeObject(gmydata);
 
-            string name = req.Query["name"];
+        return new OkObjectResult(json);
+    }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+    [FunctionName("GetShoppingCartItemById")]
+        public async Task<IActionResult> GetShoppingCartItemById(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getshoppingcartitembyid/{id}/{category}")]
+             HttpRequest req, ILogger log,string id,string category)
+                   
+        {
+            log.LogInformation($"Getting Shopping Cart Item with ID: {id}");
+            try
+            {
+                await _db.gettitembyid();
+                string getmessage="Retrived  an item successfully by Id";
+                dynamic gmydata = new ExpandoObject();
+                gmydata.message = getmessage;
+                gmydata.Data=item.Resource;
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(gmydata);
+                return new OkObjectResult(json);
+            }
+            catch (CosmosException e) when (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                string responseMessage="Invalid input params,Please check";
+                return new NotFoundObjectResult(responseMessage);
+            }
+        }
+        
 
+    [FunctionName("CreateShoppingCartItem")]
+        public async Task<IActionResult> CreateShoppingCartItems(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "createshoppingcartitem")] HttpRequest req,
+           ILogger log)
+        {
+            log.LogInformation("Creating Shopping Cart Item");
+            string requestData = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<CreateShoppingCartItem>(requestData);
+
+            var item = new ShoppingCartItem
+            {
+                ItemName = data.ItemName,
+                 Category = data.Category
+            };
+            await _db.Createitem();
+            string responsemessage="Created an item successfully";
+            dynamic cmydata = new ExpandoObject();
+            cmydata.message = responsemessage;
+            cmydata.Data=item;
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(cmydata);
+            return new OkObjectResult(json);
+        }
+
+        [FunctionName("UpdateShoppingCartItem")]
+        public async Task<IActionResult> UpdateShoppingCartItems(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "updateshoppingcartitem/{id}/{category}")] HttpRequest req,
+            ILogger log, string id,string category)
+        {
+            log.LogInformation($"Updating Shopping Cart Item with ID: {id}");
+            await _db.updateitem();
+            string requestData = await new StreamReader(req.Body).ReadToEndAsync();
+
+            var data = JsonConvert.DeserializeObject<UpdateShoppingCartItem>(requestData);
+            await _db.updateitem();
+
+            if (item.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                string responseMessage="There is no item with the mentioned id";
+                return new NotFoundObjectResult(responseMessage);
+            }
+
+            item.Resource.Collected = data.Collected;
+            await documentContainer.UpsertItemAsync(item.Resource);
+            string updatemessage="Updated successfully";
+            dynamic upmydata = new ExpandoObject();
+            upmydata.message = updatemessage;
+            upmydata.Data=item.Resource;
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(upmydata);
+            return new OkObjectResult(json);
+        }
+
+        [FunctionName("DeleteShoppingCartItem")]
+        public async Task<IActionResult> DeleteShoppingCartItems(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "delshoppingcartitem/{id}/{category}")] HttpRequest req,
+            ILogger log, string id,string category)
+        {
+            log.LogInformation($"Deleting Shopping Cart Item with ID: {id}");
+             await _db.Deleteitem();
+            string responseMessage="Deleted sucessfully";         
             return new OkObjectResult(responseMessage);
         }
     }
